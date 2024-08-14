63c56a8615006ce90ee9d68e2ee8fa6443ee2fad
gjimenez <gustavo.jimenez.crespo@gmail.com>
2024-08-13 21:17:05 -0300

    Agregar div main flex

--- public/js/touristDestine.js
+++ public/js/touristDestine.js
@@ -22,24 +22,24 @@ const getTuristDestination = async () => {
                 let img = item.img
 
                 const mainDiv = document.createElement('div');
-                mainDiv.classList.add('flex', 'h-80', 'w-full', 'm-auto', 'gap-2');
+                mainDiv.classList.add('sm:flex','sm:flex-row', 'h-80', 'w-full', 'm-auto', 'gap-2');
 
                 const subDiv1 = document.createElement('div');
-                subDiv1.classList.add('w-full' ,'basis-3/6', 'p-4');
+                subDiv1.classList.add('w-full', 'basis-3/6', 'p-4');
                 const subDiv2 = document.createElement('div');
-                subDiv2.classList.add('w-full' ,'basis-3/6', 'p-4');
+                subDiv2.classList.add('w-full', 'basis-3/6', 'p-4');
 
                 const divImg = document.createElement('div');
-                divImg.classList.add('h-full', 'bg-cover', 'bg-no-repeat', 'bg-center')
+                divImg.classList.add('border-8', 'border-sky-500','h-full', 'bg-cover', 'bg-no-repeat', 'bg-center')
                 divImg.style.backgroundImage = `url('../img/touristDestine/${img}')`;
 
                 const heading = document.createElement('h4');
-                heading.classList.add('text-4xl', 'text-center', 'text-orange-500', 'font-black', 'p-2')
+                heading.classList.add('text-xl', 'sm:text-2xl', 'md:text-3xl', 'lg:text-4xl', 'xl:text-5xl', 'text-4xl', 'text-center', 'text-orange-500', 'font-black', 'p-2')
                 heading.textContent = city;
 
 
                 const paragraph = document.createElement('p');
-                paragraph.classList.add('leading-loose', 'text-orange-500', 'text-xl', 'font-bold');
+                paragraph.classList.add('text-sm', 'sm:text-base', 'lg:text-xl', 'xl:text-2xl','leading-loose', 'text-orange-500', 'text-xl', 'font-bold');
                 paragraph.textContent = description;
 
 
@@ -70,3 +70,7 @@ const getTuristDestination = async () => {
 }
 
 getTuristDestination();
+
+//- text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl', 'text-center', 'text-orange-500', 'font-black'])
+//-         | Disfrutalo!
+//-     p(class=['overflow-hidden text-sm sm:text-base lg:text-xl xl:text-2xl',
