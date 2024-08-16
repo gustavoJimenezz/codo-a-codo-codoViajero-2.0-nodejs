8549ba0adeb079224a9977234c1df4cdc160d8ff
gjimenez <gustavo.jimenez.crespo@gmail.com>
2024-08-15 22:06:49 -0300

    Nuevos cambios

--- public/js/touristDestine.js
+++ public/js/touristDestine.js
@@ -1,67 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const index = document.getElementById('index');
-
-    if (!index) {
-        console.error('No se encontró el contenedor #cont-tourist-destine');
-        return;
-    }
-
-    const getTuristDestination = async () => {
-        try {
-            const response = await fetch('/touristDestinations', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error('Error en la respuesta de la red');
-            }
-
-            const data = await response.json();
-            let alter = 0;
-
-            data.forEach(item => {
-                if (item.active) {
-                    alter += 1;
-
-                    const { city, description, img } = item;
-
-                    const mainDiv = document.createElement('div');
-                    mainDiv.classList.add('flex', 'h-80', 'bg-slate-700', 'gap-4');
-
-                    const subDiv1 = document.createElement('div');
-                    subDiv1.classList.add('bg-white ', 'w-full','h-full', 'basis-3/6', 'p-4');
-
-                    const subDiv2 = document.createElement('div');
-                    subDiv2.classList.add('bg-white ', 'w-full','h-full', 'basis-3/6', 'p-4');
-
-
-                    //- div(class='bg-slate-700 h-80 flex gap-4')
-                    //-   div(class='bg-white w-full h-full')
-                    //-   div(class='bg-white w-full h-full')
-                    //- div(class='bg-slate-700 h-80 flex gap-4')
-                    //-   div(class='bg-white w-full h-full')
-                    //-   div(class='bg-white w-full h-full')
-                    //- div(class='bg-slate-700 h-80 flex gap-4')
-                    //-   div(class='bg-white w-full h-full')
-                    //-   div(class='bg-white w-full h-full')
-                    //- div(class='bg-slate-700 h-80 flex gap-4')
-                    //-   div(class='bg-white w-full h-full')
-                    //-   div(class='bg-white w-full h-full')
-
-     
-
-                    // mainDiv.appendChild(subDiv1);
-                    // mainDiv.appendChild(subDiv2);
-                    index.appendChild(mainDiv);
-                }
-            });
-        } catch (error) {
-            console.error("Error al obtener destinos turísticos:", error.message);
+document.addEventListener('DOMContentLoaded', async () => {
+    const index = document.getElementById('destinations');
+    try {
+      const response = await fetch('/touristDestinations', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
         }
-    };
+      });
 
-    getTuristDestination();
-});
+      if (!response.ok) {
+        throw new Error('Error en la respuesta de la red');
+      }
+      alert("emtroo!!")
+
+      const data = await response.json();
+      let order = 0;
+      data.forEach(item => {
+        if (item.active) {
+          const { city, description, img } = item;
+          order += 1;
+          const mainDiv = document.createElement('div');
+          mainDiv.classList.add('w-full', 'bg-white', 'rounded-xl', 'shadow-md', 'overflow-hidden');
+
+
+          const flexDiv = document.createElement('div');
+          flexDiv.classList.add('w-full','md:flex');
+
+          const imgDiv = document.createElement('div');
+          imgDiv.classList.add('md:shrink-0');
+
+          const imgElement = document.createElement('img');
+          imgElement.classList.add('h-48', 'w-full', 'object-cover', 'md:h-full', 'md:w-96');
+          imgElement.src = `/img/touristDestine/${img}`
+          imgElement.alt = `Image of ${city}`;
+
+          const contentDiv = document.createElement('div');
+          contentDiv.classList.add('p-8');
+
+          const titleDiv = document.createElement('div');
+          titleDiv.classList.add('uppercase', 'tracking-wide', 'text-sm', 'text-indigo-500', 'font-semibold');
+          titleDiv.textContent = city;
+
+          const descriptionParagraph = document.createElement('p');
+          descriptionParagraph.classList.add('mt-2', 'text-slate-500');
+          descriptionParagraph.textContent = description;
+
+          imgDiv.appendChild(imgElement);
+
+          contentDiv.appendChild(titleDiv);
+          contentDiv.appendChild(descriptionParagraph);
+          
+          if(order%2==0){
+            flexDiv.appendChild(imgDiv);
+            flexDiv.appendChild(contentDiv);
+          }else{
+            flexDiv.appendChild(contentDiv);
+            flexDiv.appendChild(imgDiv);
+          }
+          mainDiv.appendChild(flexDiv);
+          index.appendChild(mainDiv);
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching destinations:', error);
+    }
+  });
